cc7be29926b38fd17acef2952f03f0efe232c0c6
NBGecko <guillermo.nava.bapart@gmail.com>
2023-06-06 10:30:31 -0600

    Initial commit

---
+++ src/main/java/com/reservas/buenosapart/BuenosapartApplication.java
@@ -0,0 +1,13 @@
+package com.reservas.buenosapart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuenosapartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuenosapartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reservas/buenosapart/BuenosapartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reservas.buenosapart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuenosapartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
